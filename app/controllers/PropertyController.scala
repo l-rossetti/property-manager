3cbb86e7fe69f99c8d36c6528b1bd481b19b6444
Leonardo ROSSETTI <leonardo.rossetti@amadeus.com>
2018-10-21 01:11:11 +0200

    flow: update property

--- app/controllers/PropertyController.scala
+++ app/controllers/PropertyController.scala
@@ -19,32 +19,59 @@ class PropertyController @Inject() (repo: PropertyRepository,
                                    ) (implicit ec: ExecutionContext)
   extends MessagesAbstractController(cc) {
 
-    val createAction = "Create"
-    val updateAction = "Update"
+    object ButtonText extends Enumeration {
+        type ButtonText = Value
+
+        val Create  = Value("Create")
+        val Update  = Value("Update")
+    }
 
     /**
       * The mapping for the property form.
       */
     val propertyForm: Form[Property] = Form {
         mapping(
-            "id" -> optional(longNumber),
-            "address" -> default(nonEmptyText, "via manzoni,30"),
-            "postCode" -> default(number.verifying(min(0)), 6160),
-            "latitude" -> default(of(doubleFormat), 140.8),
-            "longitude" -> default(of(doubleFormat), 198.2),
-            "surface" -> optional(number.verifying(min(0))),
-            "bedRoomCount" -> optional(number.verifying(min(0)))
+            "id" ->             optional(longNumber),
+            "address" ->        nonEmptyText,
+            "postCode" ->       number.verifying(min(0)),
+            "latitude" ->       of(doubleFormat).verifying(min(0.0), max(100.0)),
+            "longitude" ->      of(doubleFormat).verifying(min(0.0)),
+            "surface" ->        optional(number.verifying(min(0))),
+            "bedRoomCount" ->   optional(number.verifying(min(0)))
         )(Property.apply)(Property.unapply)
     }
-    
+
+    def getPropertyForm(property: Option[Property]) = {
+        property match {
+            case None => propertyForm
+            case _    => propertyForm.fill(property.get)
+        }
+    }
     /**
       * The load property manager action.
       */
     def loadPropertyManager = Action.async { implicit request =>
-        repo.getProperties().map { properties =>  Ok(views.html.property_manager(propertyForm, createAction, properties)) }
+        val createAction = routes.PropertyController.create()
+        repo.getProperties().map {
+            properties =>  Ok(views.html.property_manager(
+                getPropertyForm(None),
+                createAction,
+                ButtonText.Create.toString,
+                properties
+            ))
+        }
     }
-    def configure(property: Property) = Action.async { implicit request =>
-        repo.getProperties().map { properties =>  Ok(views.html.property_manager(propertyForm, updateAction, properties)) }
+
+    def fillForm(property: Property) = Action.async { implicit request =>
+        val updateAction = routes.PropertyController.update(property)
+        repo.getProperties().map {
+            properties =>  Ok(views.html.property_manager(
+                getPropertyForm(Some(property)),
+                updateAction,
+                ButtonText.Update.toString,
+                properties
+            ))
+        }
     }
 
     /**
@@ -52,52 +79,73 @@ class PropertyController @Inject() (repo: PropertyRepository,
       *
       * This is asynchronous, since we're invoking the asynchronous methods on PropertyRepository.
       */
-    def createProperty = Action.async { implicit request =>
+    def create = Action.async { implicit request =>
         // Bind the form first, then fold the result, passing a function to handle errors, and a function to handle success.
         propertyForm.bindFromRequest.fold(
             // The error function. We return the index page with the error form, which will render the errors.
             // We also wrap the result in a successful future, since this action is synchronous, but we're required to return
             // a future because the property creation function returns a future.
             errorForm => {
-                    repo.getProperties().map { properties =>  Ok(views.html.property_manager(errorForm, createAction, properties)) }
+                val createAction = routes.PropertyController.create()
+                repo.getProperties().map {
+                    properties =>  Ok(views.html.property_manager(
+                        errorForm, createAction,
+                        ButtonText.Create.toString,
+                        properties
+                    ))
+                }
             },
             // There were no errors in the form, so create the property.
             property => {
                 repo.create(property).map {
                     case Success(property) =>
-                        // If successful, we simply redirect to the manage page.
-                        Redirect(routes.PropertyController.loadPropertyManager()).flashing("success" -> s"property created with id ${property.id}")
+                        // If successful, we simply redirect to the main page.
+                        Redirect(routes.PropertyController.loadPropertyManager())
+                          .flashing("success" -> s"new property created with id ${property.id.get}")
                     case Failure(e) =>
-                        Redirect(routes.PropertyController.loadPropertyManager()).flashing("error" -> s"cannot create property ${e.getMessage}")
+                        Redirect(routes.PropertyController.loadPropertyManager())
+                          .flashing("error" -> s"cannot create property ${e.getMessage}")
                 }
             }
         )
     }
 
-    def deleteProperty(id: Long) = Action.async { implicit request =>
-        //Logger.info(s"${request.body.asJson.get("id").as[String]}")
-
+    def delete(id: Long) = Action.async { implicit request =>
         repo.delete(id).map {
             case false =>
-                Logger.info(request.headers.toString())
                 Redirect(routes.PropertyController.loadPropertyManager())
                   .flashing("error" -> "property cannot be updated")
             case true =>
-                Logger.info(request.headers.toString())
                 Redirect(routes.PropertyController.loadPropertyManager())
                   .flashing("success" -> s"property with id ${id} has been deleted")
         }
     }
 
-    def updateProperty = Action.async { implicit request =>
+    /**
+      * The update property action.
+      */
+    def update(property: Property) = Action.async { implicit request =>
+
         propertyForm.bindFromRequest.fold(
             errorForm => {
-                repo.getProperties().map { properties =>  Ok(views.html.property_manager(errorForm, updateAction, properties)) }
+                val updateAction = routes.PropertyController.update(property)
+                repo.getProperties().map {
+                    properties =>  Ok(views.html.property_manager(
+                        errorForm,
+                        updateAction,
+                        ButtonText.Update.toString,
+                        properties
+                    ))
+                }
             },
             property => {
                 repo.update(property).map {
-                    case None => Redirect(routes.PropertyController.loadPropertyManager()).flashing("error" -> s"property with id ${property.id} cannot be updated")
-                    case _ => Redirect(routes.PropertyController.loadPropertyManager()).flashing("success" -> s"property with id  ${property.id} updated")
+                    case None =>
+                        Redirect(routes.PropertyController.loadPropertyManager())
+                          .flashing("error" -> s"property with id ${property.id.get} cannot be updated")
+                    case _ =>
+                        Redirect(routes.PropertyController.loadPropertyManager())
+                          .flashing("success" -> s"property with id  ${property.id.get} updated")
                 }
             }
         )
--- app/models/Property.scala
+++ app/models/Property.scala
@@ -1,8 +1,5 @@
 package models
 
-import play.api.libs.json.Json
-
-
 case class Property (id: Option[Long],
                      address: String,
                      postCode: Int,
@@ -12,5 +9,54 @@ case class Property (id: Option[Long],
                      bedRoomCount: Option[Int])
 
 object Property {
-    implicit val propertyFormat = Json.format[Property]
+
+    import play.api.mvc.QueryStringBindable
+
+    implicit def propertyBinder(implicit
+                                    optLongBinder: QueryStringBindable[Option[Long]],
+                                    stringBinder:  QueryStringBindable[String],
+                                    doubleBinder:  QueryStringBindable[Double],
+                                    optIntBinder:  QueryStringBindable[Option[Int]],
+                                    intBinder:     QueryStringBindable[Int]
+                               ) =
+        new QueryStringBindable[Property] {
+
+            override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, Property]] = {
+                for {
+                    id              <- optLongBinder.bind(key + ".id", params)
+                    address         <- stringBinder.bind(key + ".address", params)
+                    postCode        <- intBinder.bind(key + ".postCode", params)
+                    latitude        <- doubleBinder.bind(key + ".latitude", params)
+                    longitude       <- doubleBinder.bind(key + ".longitude", params)
+                    surface         <- optIntBinder.bind(key + ".surface", params)
+                    bedRoomCount    <- optIntBinder.bind(key + ".bedRoomCount", params)
+                } yield {
+                    (id, address, postCode, latitude, longitude, surface, bedRoomCount)
+                        match {
+                            case ( Right(id), Right(address), Right(postCode),
+                                   Right(latitude), Right(longitude),
+                                   Right(surface), Right(bedRoomCount))
+                                        => Right(Property(id, address, postCode, latitude, longitude,surface, bedRoomCount))
+                            case _ => Left("Unable to bind Property")
+                    }
+                }
+            }
+
+            override def unbind(key: String, p: Property): String =
+                optLongBinder.unbind(key + ".id", p.id) + "&" +
+                stringBinder.unbind(key + ".address", p.address) + "&" +
+                intBinder.unbind(key + ".postCode", p.postCode) + "&" +
+                doubleBinder.unbind(key + ".latitude", p.latitude) + "&" +
+                doubleBinder.unbind(key + ".longitude", p.longitude) +
+                ( p.surface match {
+                    case None => ""
+                    case _    => "&" + optIntBinder.unbind(key + ".surface", p.surface)
+                }) +
+                ( p.bedRoomCount match {
+                    case None => ""
+                    case _    => "&" + optIntBinder.unbind(key + ".bedRoomCount", p.bedRoomCount)
+                })
+
+
+        }
 }
